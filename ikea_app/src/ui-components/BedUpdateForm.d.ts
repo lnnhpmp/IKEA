f9e9d812313f7697faca9c9cffb28e4e23d7e1f1
lnnhpmp <xufangyu709@gmail.com>
2023-11-04 10:16:32 +0100

    react setup

--- ikea_app/src/ui-components/BedUpdateForm.d.ts
+++ ikea_app/src/ui-components/BedUpdateForm.d.ts
@@ -6,37 +6,48 @@
 
 import * as React from "react";
 import { GridProps, TextFieldProps } from "@aws-amplify/ui-react";
-import { EscapeHatchProps } from "@aws-amplify/ui-react/internal";
+import { Bed } from "../models";
+export declare type EscapeHatchProps = {
+    [elementHierarchy: string]: Record<string, unknown>;
+} | null;
+export declare type VariantValues = {
+    [key: string]: string;
+};
+export declare type Variant = {
+    variantValues: VariantValues;
+    overrides: EscapeHatchProps;
+};
 export declare type ValidationResponse = {
     hasError: boolean;
     errorMessage?: string;
 };
 export declare type ValidationFunction<T> = (value: T, validationResponse: ValidationResponse) => ValidationResponse | Promise<ValidationResponse>;
-export declare type BedCreateFormInputValues = {
+export declare type BedUpdateFormInputValues = {
     width?: number;
     height?: number;
     depth?: number;
 };
-export declare type BedCreateFormValidationValues = {
+export declare type BedUpdateFormValidationValues = {
     width?: ValidationFunction<number>;
     height?: ValidationFunction<number>;
     depth?: ValidationFunction<number>;
 };
 export declare type PrimitiveOverrideProps<T> = Partial<T> & React.DOMAttributes<HTMLDivElement>;
-export declare type BedCreateFormOverridesProps = {
-    BedCreateFormGrid?: PrimitiveOverrideProps<GridProps>;
+export declare type BedUpdateFormOverridesProps = {
+    BedUpdateFormGrid?: PrimitiveOverrideProps<GridProps>;
     width?: PrimitiveOverrideProps<TextFieldProps>;
     height?: PrimitiveOverrideProps<TextFieldProps>;
     depth?: PrimitiveOverrideProps<TextFieldProps>;
 } & EscapeHatchProps;
-export declare type BedCreateFormProps = React.PropsWithChildren<{
-    overrides?: BedCreateFormOverridesProps | undefined | null;
+export declare type BedUpdateFormProps = React.PropsWithChildren<{
+    overrides?: BedUpdateFormOverridesProps | undefined | null;
 } & {
-    clearOnSuccess?: boolean;
-    onSubmit?: (fields: BedCreateFormInputValues) => BedCreateFormInputValues;
-    onSuccess?: (fields: BedCreateFormInputValues) => void;
-    onError?: (fields: BedCreateFormInputValues, errorMessage: string) => void;
-    onChange?: (fields: BedCreateFormInputValues) => BedCreateFormInputValues;
-    onValidate?: BedCreateFormValidationValues;
+    id?: string;
+    bed?: Bed;
+    onSubmit?: (fields: BedUpdateFormInputValues) => BedUpdateFormInputValues;
+    onSuccess?: (fields: BedUpdateFormInputValues) => void;
+    onError?: (fields: BedUpdateFormInputValues, errorMessage: string) => void;
+    onChange?: (fields: BedUpdateFormInputValues) => BedUpdateFormInputValues;
+    onValidate?: BedUpdateFormValidationValues;
 } & React.CSSProperties>;
-export default function BedCreateForm(props: BedCreateFormProps): React.ReactElement;
+export default function BedUpdateForm(props: BedUpdateFormProps): React.ReactElement;
